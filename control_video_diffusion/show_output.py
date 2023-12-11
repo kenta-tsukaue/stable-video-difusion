131f7e12b3a0101c43dbb7e7e4ac7c1e4e6666b2
kenta-tsukaue <tsukaue.k@arklet.jp>
2023-12-11 18:11:17 +0900

    add

--- control_video_diffusion/show_output.py
+++ control_video_diffusion/show_output.py
@@ -13,7 +13,7 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:64'
 
 
-def decode_latents(vae, latents, num_frames, decode_chunk_size=14):
+"""def decode_latents(vae, latents, num_frames, decode_chunk_size=14):
         # [batch, frames, channels, height, width] -> [batch*frames, channels, height, width]
         latents = latents.flatten(0, 1)
 
@@ -42,21 +42,61 @@ def decode_latents(vae, latents, num_frames, decode_chunk_size=14):
 
         # we always cast to float32 as this does not cause significant overhead and is compatible with bfloat16
         frames = frames.float()
-        return frames
+        return frames"""
 
+def decode_vae_latent(vae, latents, device):
+    #print("video.size()", video.size())
+    # 元のビデオの形状を保存
+    batch_size, num_frames, channels, height, width = latents.size()
+
+    video_list = []
+
+    # バッチごとにループ
+    for batch_idx in range(batch_size):
+        #print(batch_idx)
+        batch_latents = []
+
+        # 各フレームを個別に処理
+        for frame_idx in range(num_frames):
+            #print(batch_idx, frame_idx)
+            # フレームを取り出す
+            frame_latent = latents[batch_idx, frame_idx, :, :, :].unsqueeze(0).to(device=device)
+            
+            # VAEを使用してエンコード
+            frame = vae.decode(frame_latent).sample
+
+            # 処理結果をリストに追加
+            batch_latents.append(frame.cpu())
+
+            # 不要なテンソルの削除
+            del frame
+            del frame_latent
+
+            # GPUメモリキャッシュのクリア
+            torch.cuda.empty_cache()
+
+            # 各バッチのフレームを結合
+            #print("torch.stack(batch_latents, dim=1)", torch.stack(batch_latents, dim=1).size())
+        
+        video_list.append(torch.stack(batch_latents, dim=1))
+
+    # 全バッチを結合
+
+    video = torch.cat(video_list, dim=0).to(device=device)
+
+    return video
 
 # 読み込むファイル名
 file_name = './output_latents.pkl'
-vae = getModel("vae")
-
+vae = getModel("vae").to(device).to(dtype=torch.float16)
 
 
 # Pickleファイルから読み込み
 with open(file_name, 'rb') as f:
-    latents = pickle.load(f).to("cpu").to(dtype=torch.float32)
+    latents = pickle.load(f).to(device).to(dtype=torch.float16)
 
 print(latents.size())
-frames = decode_latents(vae, latents, 14, 14)
+frames = decode_vae_latent(vae, latents, device)
 
 
 # 画像のリストを取得
