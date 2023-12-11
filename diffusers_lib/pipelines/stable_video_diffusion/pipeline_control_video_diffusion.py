a19a336d452073444894036cf5405f02b91840c1
kenta-tsukaue <tsukaue.k@arklet.jp>
2023-12-11 15:04:56 +0900

    delete cache

--- diffusers_lib/pipelines/stable_video_diffusion/pipeline_control_video_diffusion.py
+++ diffusers_lib/pipelines/stable_video_diffusion/pipeline_control_video_diffusion.py
@@ -629,6 +629,12 @@ class ControlVideoDiffusionPipeline(DiffusionPipeline):
 
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
                     progress_bar.update()
+            
+        #del frame
+        #del frame_latent
+
+        # GPUメモリキャッシュのクリア
+        torch.cuda.empty_cache()
 
         if not output_type == "latent":
             # cast back to fp16 if needed
